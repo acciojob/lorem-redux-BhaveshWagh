ed852ecec84bcd62b7cc24116b8c98493f6d4d20
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-08-30 16:59:24 +0530

    done 1.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect } from 'react'
+import './../styles/App.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchPosts } from '../redux'
+import Post from './Post'
+import Posts from './Posts'
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+	const dispatch = useDispatch()
+	const { posts, isLoading } = useSelector((state) => state)
+
+	useEffect(() => {
+		dispatch(fetchPosts())
+	}, [])
+	return (
+		<div>
+			<h1>A short Naration of Lorem Ipsum</h1>
+			<h4>
+				Below Contains A title and Body gotten froma random API, Please take
+				your time to Review
+			</h4>
+			<Posts isLoading={isLoading} posts={posts} />
+		</div>
+	)
 }
 
 export default App
